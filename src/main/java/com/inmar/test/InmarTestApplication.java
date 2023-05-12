5dc0a364e4fe0f7453d9d0e53f917c9e8b2d462c
ramathogaru <95012330+ramathogaru@users.noreply.github.com>
2023-05-12 20:31:50 +0530

    Add files via upload

--- src/main/java/com/inmar/test/InmarTestApplication.java
+++ src/main/java/com/inmar/test/InmarTestApplication.java
@@ -1,1 +1,13 @@
+package com.inmar.test;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmarTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmarTestApplication.class, args);
+	}
+
+}
